655b65afbe9d7e2a1a484e2bb4619810ad57720c
BlankTempest <61834898+BlankTempest@users.noreply.github.com>
2023-04-09 10:56:06 +0530

    fix

--- Tetris_client.mk
+++ Tetris_client.mk
@@ -1,5 +1,5 @@
 SOURCE = src/main_client.c src/init.c src/defs.c src/input.c src/draw.c src/piece.c src/board.c src/tetris.c src/client.c
-LIBRARY = -lmingw32 -lSDL2main -lSDL2 -lSDL2_mixer -lSDL2_image -lSDL2_net -lSDL2_ttf
+LIBRARY = -lmingw32 -lSDL2main -lSDL2 -lSDL2_mixer -lSDL2_image -lSDL2_net
 BUILD = build/tetris_client
 
 all:
